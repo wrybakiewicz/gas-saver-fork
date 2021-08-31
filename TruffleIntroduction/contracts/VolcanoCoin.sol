8b95c49f26bcf25f145c52561e26089f3d1b92ef
extropyCoder <laurence@extropy.io>
2021-08-31 14:37:15 +0100

    unit test

--- TruffleIntroduction/contracts/VolcanoCoin.sol
+++ TruffleIntroduction/contracts/VolcanoCoin.sol
@@ -1,46 +1,50 @@
 // SPDX-License-Identifier: UNLICENSED
-
 pragma solidity ^0.8.0;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+contract VolcanoCoin {
+    uint256 public totalSupply;
+    address owner;
 
-contract VolcanoCoin is ERC20("Volcano", "VLC"), Ownable {
-    uint256 tokenSupply = 100;
-    event tokenSupplyChange(uint256);
     mapping(address => uint256) public balances;
-    mapping(address => Payment[]) public paymentsRecord;
-
+    mapping(address => Payment[]) payments;
+    
     struct Payment {
-        uint256 amount;
-        address recipient;
+      address receipient;
+      uint256 amount;
     }
-
-    constructor() {
-        _mint(msg.sender, tokenSupply);
+    
+    modifier onlyOwner {
+        require(msg.sender == owner,"You must be the owner"); 
+        _; 
     }
-
-    function getTokenSupply() public view returns (uint256) {
-        return tokenSupply;
-    }
-
-    function changeTokenSupply() public onlyOwner {
-        tokenSupply = 300;
-        emit tokenSupplyChange(tokenSupply);
-    }
-
-    function mintTokens() public onlyOwner {
-        _mint(msg.sender, tokenSupply);
-    }
-
-    function makeTransfer(address _recipient, uint256 _amount) public {
-        _transfer(msg.sender, _recipient, _amount);
-        balances[msg.sender] -= _amount;
-        addPaymentRecord(_recipient, _amount);
-    }
-
-    function addPaymentRecord(address _recipient, uint256 _amount) internal {
-        Payment memory newPayment = Payment(_amount, _recipient);
-        paymentsRecord[msg.sender].push(newPayment);
-    }
+   
+    event supplyChanged(uint256);
+    event Transfer(address indexed, uint256);
+   
+   constructor() {
+      totalSupply = 10000;
+      owner = msg.sender;
+      balances[msg.sender] = totalSupply;
+   }
+   
+   
+   function updateTotalSupply() private onlyOwner {
+      totalSupply = totalSupply + 1000;
+      emit supplyChanged(totalSupply);
+   }
+   
+   function transfer(address _recipient, uint256 _amount) public {
+      require(balances[msg.sender] >= _amount,"Insufficient Balance");
+      balances[msg.sender] -= _amount;
+      balances[_recipient] += _amount;
+      emit Transfer(_recipient, _amount);
+      Payment memory payment;
+      payment.receipient = _recipient;
+      payment.amount = _amount;
+      payments[msg.sender].push(payment);
+   }
+     
+   function getPayments(address _user) public view returns (Payment[] memory) {
+       return payments[_user];
+   }
 }
