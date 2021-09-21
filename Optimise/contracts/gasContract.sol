52a5b04c191caf39eb2e975fec46ba672c289afc
extropyCoder <laurence@extropy.io>
2021-09-21 07:21:34 +0000

    revert and event tests

--- Optimise/contracts/gasContract.sol
+++ Optimise/contracts/gasContract.sol
@@ -4,8 +4,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract GasContract is Ownable {
 
-    uint256 public totalSupply;
-    uint256 paymentCounter;
+    uint256 public totalSupply = 0;
+    uint256 paymentCounter = 1;
 
     address [5] public administrators;
     enum PaymentType { Unknown, BasicPayment, Refund, Dividend, GroupPayment }
@@ -20,26 +20,26 @@ contract GasContract is Ownable {
       uint256 paymentID;
       PaymentType paymentType;
       address recipient;
-      string recipientName;  // max 10 characters
+      string recipientName;  // max 12 characters
       uint256 lastUpdate;
       address updatedBy;
       uint256 amount;
       
-      
-
     }
 
     modifier onlyAdmin {
-        require (checkForAdmin(msg.sender), "Caller not admin" );
+        require (checkForAdmin(msg.sender), "Gas Contract -  Caller not admin" );
         _;
     }
 
-    event supplyChanged(uint256);
+    event supplyChanged(address indexed, uint256 indexed);
     event Transfer(address indexed, uint256);
-   
+    event PaymentUpdated(address indexed admin, uint256 indexed ID, uint256 indexed amount, string  recipient);
+
+
    constructor(address[] memory _admins) {
       totalSupply = 10000;
-       balances[msg.sender] = totalSupply;
+      balances[msg.sender] = totalSupply;
       setUpAdmins(_admins);
    }
    
@@ -58,7 +58,7 @@ contract GasContract is Ownable {
    function updateTotalSupply() public onlyOwner {
       totalSupply = totalSupply + 1000;
       balances[msg.sender] = totalSupply;
-      emit supplyChanged(totalSupply);
+      emit supplyChanged(msg.sender,totalSupply);
    }
 
    function checkForAdmin(address _user) public view returns (bool) {
@@ -71,8 +71,9 @@ contract GasContract is Ownable {
        return admin;
    }
    
-   function transfer(address _recipient, uint256 _amount) public {
-      require(balances[msg.sender] >= _amount,"Transfer function - Sender has insufficient Balance");
+   function transfer(address _recipient, uint256 _amount, string memory _name) public {
+      require(balances[msg.sender] >= _amount,"Gas Contract - Transfer function - Sender has insufficient Balance");
+      require(bytes(_name).length < 13,"Gas Contract - Transfer function -  Name too long");
       balances[msg.sender] -= _amount;
       balances[_recipient] += _amount;
       emit Transfer(_recipient, _amount);
@@ -80,11 +81,15 @@ contract GasContract is Ownable {
       payment.paymentType = PaymentType.BasicPayment;
       payment.recipient = _recipient;
       payment.amount = _amount;
+      payment.recipientName = _name;
       payment.paymentID = ++paymentCounter;
       payments[msg.sender].push(payment);
    }
      
     function updatePayment(address _user, uint256 _ID, uint128 _amount) public onlyAdmin {
+        require(_ID > 0,"Gas Contract - Update Payment function - ID must be greater than 0");
+        require(_amount > 0,"Gas Contract - Update Payment function - Amount must be greater than 0");
+        require(_user != address(0) ,"Gas Contract - Update Payment function - Administrator must have a valid non zero address");
         for (uint256 ii=0;ii<payments[_user].length;ii++){
             Payment memory thisPayment = payments[_user][ii];
             uint256 lastUpdate = block.timestamp;
@@ -93,8 +98,9 @@ contract GasContract is Ownable {
             if(thisPayment.paymentID==_ID){
                payments[_user][ii].lastUpdate =  lastUpdate;
                payments[_user][ii].updatedBy = updatedBy;
-                payments[_user][ii].amount = _amount;
-                lastUpdateRecord[msg.sender] = block.timestamp;
+               payments[_user][ii].amount = _amount;
+               lastUpdateRecord[msg.sender] = block.timestamp;
+               emit PaymentUpdated(msg.sender, _ID, _amount,payments[_user][ii].recipientName);
             }
         }
     }
--- Optimise/test/gas.test.js
+++ Optimise/test/gas.test.js
@@ -59,40 +59,55 @@ contract("Gas", accounts => {
     it("should send a basic payment", async () => {
             const instance = await GasContract.deployed();
             const tx = await instance.transfer(accounts[1],100, {from : accounts[0]});   
-            const payments  = await instance.getPayments(accounts[0]);   
-            assert.equal(payments[0].paymentType,1);
-            assert.equal(payments[0].recipient,accounts[1]);
-            assert.equal(payments[0].recipientName,'');
-            assert.equal(payments[0].amount,100);
-       });   
+            const payments  = await instance.getPayments(accounts[0]);  
+            const lastPayment = payments.length -1;  
+            assert.equal(payments[lastPayment].paymentType,1);
+            assert.equal(payments[lastPayment].recipient,accounts[1]);
+            assert.equal(payments[lastPayment].recipientName,'');
+            assert.equal(payments[lastPayment].amount,100);
+       });  
+
+       it("should send emit an event", async () => {
+        const instance = await GasContract.deployed();
+        const tx = await instance.transfer(accounts[1],100, {from : accounts[0]});   
+        // truffleAssert.eventEmitted(result, 'TestEvent', { param1: 10, param2: 20 });
+        await truffleAssert.eventEmitted(tx, 'Transfer');
+  
+    });  
 
        it("updates a payment", async () => {
         const instance = await GasContract.deployed();
-        const tx = await instance.transfer(accounts[2],13, {from : accounts[1]});   
+        const tx1 = await instance.transfer(accounts[1],100, "account 1", {from : accounts[0]}); 
+        const tx2 = await instance.transfer(accounts[2],13, "account 2", {from : accounts[1]});   
         let payments  = await instance.getPayments(accounts[1]); 
-        const lastPaymnet = payments.length -1;
-        assert.equal(payments[lastPaymnet].paymentType,1);
-        assert.equal(payments[lastPaymnet].recipient,accounts[2]);
-        assert.equal(payments[lastPaymnet].recipientName,'');
-        assert.equal(payments[lastPaymnet].amount,13);
-        const paymentID = payments[lastPaymnet].paymentID;
+        const lastPayment = payments.length -1;
+//        console.log("lastPayment is " + lastPayment)
+        assert.equal(payments[lastPayment].paymentType,1);
+        assert.equal(payments[lastPayment].recipient,accounts[2]);
+        assert.equal(payments[lastPayment].recipientName,"account 2");
+        assert.equal(payments[lastPayment].amount,13);
+        const paymentID = payments[lastPayment].paymentID;
 
        // now update the payment
-       const tx2 = await  instance.updatePayment(accounts[1], paymentID, 19, {from : accounts[0]});  
+       const tx3 = await  instance.updatePayment(accounts[1], paymentID, 19, {from : accounts[0]});  
        payments  = await instance.getPayments(accounts[1]); 
-       assert.equal(payments[lastPaymnet].paymentType,1);
-       assert.equal(payments[lastPaymnet].recipient,accounts[2]);
-       assert.equal(payments[lastPaymnet].recipientName,'');
-       assert.equal(payments[lastPaymnet].amount,19);
-
+       assert.equal(payments[lastPayment].paymentType,1);
+       assert.equal(payments[lastPayment].recipient,accounts[2]);
+       assert.equal(payments[lastPayment].recipientName,"account 2");
+       assert.equal(payments[lastPayment].amount,19);
+       await truffleAssert.eventEmitted(tx3, 'PaymentUpdated');
 
   });   
 
+  it("Fails to transfer  - name too long", async () => {
+    const instance = await GasContract.deployed();
+   await truffleAssert.fails(instance.transfer(accounts[1],100, "1234567890123", {from : accounts[0]}) , truffleAssert.ErrorType.REVERT);  
 
+
+});  
        it("Fails to update payment - non admin", async () => {
          const instance = await GasContract.deployed();
-        await truffleAssert.reverts(instance.updatePayment(accounts[1], 1, 13, {from : accounts[8]}),"Caller not admin");  
-
+        await truffleAssert.fails(instance.updatePayment(accounts[1], 1, 13, {from : accounts[8]}),truffleAssert.ErrorType.REVERT);  
 
    });   
 
@@ -101,31 +116,34 @@ contract("Gas", accounts => {
         const initialSupply = await instance.totalSupply.call();
         const tx = await instance.updateTotalSupply({from : accounts[0]});
         const finalSupply = await instance.totalSupply.call();
-        assert.equal(finalSupply.toNumber(), initialSupply.toNumber() + 1000,"Update supply failed",);  
+        assert.equal(finalSupply.toNumber(), initialSupply.toNumber() + 1000,"Update supply failed");  
 
-        let adminFlag;
-           adminFlag = await instance.checkForAdmin.call(accounts[8]);
-           assert.equal(
-               adminFlag,
-           false,
-               "Incorrect admin");    
+        const welcome = await instance.welcome.call();
+        assert.equal(welcome,"hello !","Incorrect Welcome message");  
 
-        const tx2 = await instance.transfer(accounts[2],3, {from : accounts[1]});   
+        let adminFlag = await instance.checkForAdmin.call(accounts[8]);
+        assert.equal(adminFlag,false,"Incorrect admin");    
+        
+        const tx1 = await instance.transfer(accounts[1],100, "account 1", {from : accounts[0]}); 
+        const tx2 = await instance.transfer(accounts[2],1,"account 2", {from : accounts[1]});   
+        const tx3 = await instance.transfer(accounts[2],2,"account 2", {from : accounts[1]});   
+        const tx4 = await instance.transfer(accounts[2],3,"account 2", {from : accounts[1]});   
+
         let payments  = await instance.getPayments(accounts[1]); 
-        const lastPaymnet = payments.length -1;
-        assert.equal(payments[lastPaymnet].paymentType,1);
-        assert.equal(payments[lastPaymnet].recipient,accounts[2]);
-        assert.equal(payments[lastPaymnet].recipientName,'');
-        assert.equal(payments[lastPaymnet].amount,3);
-        const paymentID = payments[lastPaymnet].paymentID;
+        const lastPayment = payments.length -1;
+        assert.equal(payments[lastPayment].paymentType,1);
+        assert.equal(payments[lastPayment].recipient,accounts[2]);
+        assert.equal(payments[lastPayment].recipientName,"account 2");
+        assert.equal(payments[lastPayment].amount,3);
+        const paymentID = payments[lastPayment].paymentID;
 
 
-       const tx3 = await  instance.updatePayment(accounts[1], paymentID, 14, {from : accounts[0]});  
+       const tx5= await  instance.updatePayment(accounts[1], paymentID, 14, {from : accounts[0]});  
        payments  = await instance.getPayments(accounts[1]); 
-       assert.equal(payments[lastPaymnet].paymentType,1);
-       assert.equal(payments[lastPaymnet].recipient,accounts[2]);
-       assert.equal(payments[lastPaymnet].recipientName,'');
-       assert.equal(payments[lastPaymnet].amount,14);
+       assert.equal(payments[lastPayment].paymentType,1);
+       assert.equal(payments[lastPayment].recipient,accounts[2]);
+       assert.equal(payments[lastPayment].recipientName,"account 2");
+       assert.equal(payments[lastPayment].amount,14);
 
 
    });   
