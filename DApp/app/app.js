53b93d5fda542fb5b78027fa6944f646ad57657f
Tom Dwyer <tom.dwyer@extropy.io>
2021-11-22 13:07:55 +0000

    Added most simple dapp

--- DApp/app/app.js
+++
@@ -1,6 +0,0 @@
-// Library dependency
-import Web3 from 'web3';
-
-// Smart contract ABI
-const myNft = require('./contracts/erc721.json');
-
--- DApp/dapp/contracts/EncodeErc721.sol
+++ DApp/dapp/contracts/EncodeErc721.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract EncodeToken is ERC721URIStorage {
+contract EncodeErc721 is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("EncodeToken", "ENC") {}
+    constructor() ERC721("EncodeErc721", "ENC") {}
 
     function mintToken(address tokenOwner, string memory tokenURI)
         public
