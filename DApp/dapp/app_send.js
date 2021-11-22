bf8f2db6bccf1944053c01298d180da9c4a08ec9
Tom Dwyer <tom.dwyer@extropy.io>
2021-11-22 13:28:35 +0000

    Rejigged code

--- DApp/dapp/app_send.js
+++ DApp/dapp/app_send.js
@@ -10,20 +10,20 @@ const web3 = new Web3("http://127.0.0.1:8545");
 // Set variable here for reuse
 const ownerPub = '0x536F8222C676b6566FF34E539022De6c1c22cc06';
 
+// Add credentials
+web3.eth.accounts.wallet.add({
+    privateKey: '4e05f623e5e7a057db5b80d8f3ae73f9efac3595ae02efca0df1f0e25222439b',
+    address: ownerPub
+});
+
+// Initialise contract
+const encodeNft = new web3.eth.Contract(nftAbi.abi, "0xA97409103E409f93ecB7599C8d2f13a1845049EB");
+
 (async () => {
-    // Add credentials
-    web3.eth.accounts.wallet.add({
-        privateKey: '4e05f623e5e7a057db5b80d8f3ae73f9efac3595ae02efca0df1f0e25222439b',
-        address: ownerPub
-    })
-
-    // Initialise contract
-    const encodeNft = new web3.eth.Contract(nftAbi.abi, "0xA97409103E409f93ecB7599C8d2f13a1845049EB")
-
     // Get contract name
     const mint = await encodeNft.methods
         .mintToken(ownerPub, "abc.com")
-        .send({ from: ownerPub, gas: 500000 })
+        .send({ from: ownerPub, gas: 500000 });
 
     // Log tx info
     console.log(mint);
