5c439775f28176250aae294715c5c4bda1c3c770
Tom Dwyer <tom.dwyer@extropy.io>
2021-11-29 14:20:57 +0000

    DApp from answers

--- DApp/dapp/migrations/1_initial_migration.js
+++ DApp/dapp/migrations/1_initial_migration.js
@@ -1,8 +1,5 @@
 const Migrations = artifacts.require("Migrations");
-const EncodeErc721 = artifacts.require("EncodeErc721");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
-  deployer.deploy(EncodeErc721);
 };
-
--- DApp/dapp/migrations/2_encode_migrations.js
+++
@@ -1,6 +0,0 @@
-const EncodeErc721 = artifacts.require("EncodeErc721");
-
-module.exports = async function (deployer, network, accounts) {
-    // deployment steps
-    await deployer.deploy(EncodeErc721);
-};
