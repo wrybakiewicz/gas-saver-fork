bff399b8c6e017ad008a07dba4dc43ab67fc019f
extropyCoder <laurence@extropy.io>
2021-12-16 09:53:46 +0000

    test script

--- DeFiHardhat/test/sample-test.js
+++ DeFiHardhat/test/sample-test.js
@@ -1,19 +1,77 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const DeFi = artifacts.require("DeFi");
+const DAIMock = artifacts.require("DAIMock"); 
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+const DAIAddress = "0x6b175474e89094c44da98b954eedeac495271d0f";
+const USDCAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+const COINBASE = "0x503828976D22510aad0201ac7EC88293211D23Da";
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+const uniswapRouter = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
+
+
+
+contract("DeFi", accounts => {
+let owner;
+let DAI_TokenContract;
+let USDC_TokenContract;
+let DeFi_Instance;
+const INITIAL_AMOUNT = 999999999000000;
+    before(async function () {
+        accounts = await web3.eth.getAccounts();
+        owner = accounts[0];
+        console.log("owner account is " , owner);
+
+         DAI_TokenContract = await DAIMock.at(DAIAddress);
+         USDC_TokenContract = await DAIMock.at(USDCAddress);
+        const symbol = await DAI_TokenContract.symbol();
+        console.log(symbol);
+        await DAI_TokenContract.transfer(owner, BigInt(INITIAL_AMOUNT), {
+            from: COINBASE,
+            gasPrice: 0,
+          })
+          
+          DeFi_Instance = await DeFi.deployed();
+    });
+
+
+    it("should check transfer succeeded", async () => {
+        let DAI_TokenContract = await DAIMock.at(DAIAddress);
+        const result = await DAI_TokenContract.balanceOf(owner);
+        assert( result.toNumber() > 0); 
+    });
+    it("should test the router", async () => {
+
+        const factoryAddress = await DeFi_Instance.swapRouter();
+
+    });
+    it("should sendDAI to contract", async () => {
+
+        await DAI_TokenContract.transfer(DeFi_Instance.address, BigInt(INITIAL_AMOUNT), {
+            from: owner,
+            gasPrice: 0,
+          })
+
+    });
+    it("should make a swap", async () => {
+        let initialUSDC = await USDC_TokenContract.balanceOf(owner);
+        console.log(initialUSDC.toNumber());
+ 
+        await DeFi_Instance.swapDAItoUSDC(99999999900000, {
+            from: owner,
+            gasPrice: 0,
+          })
+          let finalUSDC = await USDC_TokenContract.balanceOf(owner);
+          console.log(finalUSDC.toNumber());
+
+    });
+    it.only("should get the ETH price", async () => {
+
+        let ETHPrice = await DeFi_Instance.getETHPrice();
+
+          console.log(ETHPrice.toNumber());
+
+    });
+ 
 });
