4dec80604fdaf797dc6bf0b50f36b5f187644ba7
Karol <karol@extropy.io>
2022-05-05 13:50:34 +0100

    Gas game

---
+++ GasOptimisation/README.md
@@ -0,0 +1,13 @@
+# Optimisation Game Instructions
+
+1. Run ` npm i` to install the dependencies
+2. Run ` npx hardhat test` to run the tests and get a gas usage report
+
+![](https://i.imgur.com/a/q3RjSjw)
+
+The aim of the game is to reduce the Average figures for contract deployment and transfer and updatePayment functions as much as possible.
+
+You can change the contract as much as you like.
+You **cannot** \* change the tests, and all the tests must pass.
+
+\* you can slightly adjust tests for whitelist functions if you need so.
--- GasOptimisation/hardhat.config.js
+++ GasOptimisation/hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
-
+require("hardhat-gas-reporter");
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -17,8 +17,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
-  networks: {
-    hardhat: {},
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.0",
+      },
+    ],
+  },
+  gasReporter: {
+    currency: "USD",
   },
 };
